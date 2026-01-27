25b65ba89d8c93bc2ebaead2f4bc09b7906ab89f
Tom Sileo <tsileo@vivaldi.net>
2026-01-26 23:36:09 -0500

    tls mongo connection

--- src/db/index.js
+++ src/db/index.js
@@ -5,7 +5,7 @@ const connect = async() => {
     try {
         const user = process.env.MONGODB_USERNAME;
         const pass = process.env.MONGODB_PASSWORD;
-        const conn = await mongoose.connect(process.env.MONGODB_URL, {user: user, pass: pass});
+        const conn = await mongoose.connect(process.env.MONGODB_URL, {user: user, pass: pass, tls: true, tlsAllowInvalidCertificates: process.env.ALLOW_INVALID_CERTS, tlsCAFile: process.env.CA_FILE_PATH});
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (err) { 
         console.error(`Error: ${err.message}`)
